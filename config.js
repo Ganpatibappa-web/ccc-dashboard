da5d0bfc71867e85a9ed20321f17e16b8a610a15
Ganpatibappa-web <bappawork0@gmail.com>
2026-06-30 11:39:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,22 @@
+//==============================================
+// CCC Dashboard Configuration
+//==============================================
 
+// Google Apps Script API URL
+const API_URL =
+"https://script.google.com/macros/s/AKfycbxNmHbnECFZZSIlEUX7EP9G8D6-zbMol8nJ5LOB7cB08zncmaerSDF11Y8qsqN5FrD7/exec";
+
+
+// Application Title
+const APP_TITLE =
+"CCC Wise Recovery Dashboard";
+
+
+// Version
+const APP_VERSION =
+"3.0.0";
+
+
+// Developer
+const DEVELOPER =
+"Ganpatibappa-web";
